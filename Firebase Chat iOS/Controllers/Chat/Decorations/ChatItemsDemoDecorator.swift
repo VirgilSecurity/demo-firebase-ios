fb9631f0cf63b969abe1fb97eb4bd96df74f3c50
Eugen Pivovarov <epivovarov@virgilsecurity.com>
2018-09-28 16:30:54 +0300

    Updated dependencies

--- Firebase Chat iOS/Controllers/Chat/Decorations/ChatItemsDemoDecorator.swift
+++ Firebase Chat iOS/Controllers/Chat/Decorations/ChatItemsDemoDecorator.swift
@@ -73,13 +73,18 @@ final class ChatItemsDemoDecorator: ChatItemsDecoratorProtocol {
                 }
             }
 
+            let messageDecorationAttributes = BaseMessageDecorationAttributes(canShowFailedIcon: true,
+                                                                              isShowingTail: showsTail,
+                                                                              isShowingAvatar: showsTail,
+                                                                              isShowingSelectionIndicator: false,
+                                                                              isSelected: false)
+            let decorationAttributes = ChatItemDecorationAttributes(bottomMargin: bottomMargin,
+                                                                    messageDecorationAttributes: messageDecorationAttributes)
+
             decoratedChatItems.append(
                 DecoratedChatItem(
                     chatItem: chatItem,
-                    decorationAttributes: ChatItemDecorationAttributes(bottomMargin: bottomMargin,
-                                                                       canShowTail: showsTail,
-                                                                       canShowAvatar: showsTail,
-                                                                       canShowFailedIcon: true)
+                    decorationAttributes: decorationAttributes
                 )
             )
             decoratedChatItems.append(contentsOf: additionalItems)
--- Podfile
+++ Podfile
@@ -1,5 +1,4 @@
-# Uncomment the next line to define a global platform for your project
-# platform :ios, '9.0'
+platform :ios, '10.0'
 
 target 'Firebase Chat iOS' do
   # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
@@ -10,11 +9,11 @@ target 'Firebase Chat iOS' do
   pod 'Firebase/Auth'
   pod 'Firebase/Firestore'
 
-  pod 'VirgilCryptoApiImpl', '~> 3.0.0'
-  pod 'VirgilSDK', '~> 5.0.0'
+  pod 'VirgilCryptoApiImpl', '~> 3.0'
+  pod 'VirgilSDK', '~> 5.0'
 
-  pod 'Chatto'
-  pod 'ChattoAdditions'
+  pod 'Chatto', '~> 3.3.1'
+  pod 'ChattoAdditions', '~> 3.3.1'
 
   pod 'PKHUD', '~> 5.0'
 end
